0b77de2ed719103e89edcd8d01d8ad92531d1af8
Shahabr99 <sean.rohani9@gmail.com>
2023-03-30 23:07:33 -0700

    almost done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import satisfaction_survey as survey
 
 app=Flask(__name__)
 
@@ -8,13 +8,25 @@ app.config['SECRET_KEY']='Codingforlife'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 debug = DebugToolbarExtension(app)
 
-requests = []
+answers = []
+
+@app.route('/answer', methods=['POST'])
+def store_answer():
+  respond = request.form['shopped-before']
+  answers.append(respond)
+
+  if (len(answers) == len(survey.questions)):
+    return redirect('/complete')
+  else:
+    return redirect(f'/question/{len(answers)}')
+
+  
 
 @app.route('/')
 def go_home():
 
-  title = satisfaction_survey.title
-  instructions = satisfaction_survey.instructions
+  title = survey.title
+  instructions = survey.instructions
 
   return render_template('home.html', title = title, instructions = instructions)
 
@@ -22,7 +34,14 @@ def go_home():
 @app.route('/question/<int:number>')
 def show_question(number):
   
-  question = satisfaction_survey.questions[number]
-  
+  question = survey.questions[number]
 
-  return render_template('question.html', question=question)
+  if (len(answers) != number):
+    flash(f'Invalid question {number}') 
+    return redirect('/question/0')  
+  else:
+    return render_template('question.html', question=question)
+
+@app.route('/complete')
+def completion():
+  return render_template('complete.html')
